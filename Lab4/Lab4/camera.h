f8530b11c4d7afa2d30a44fc2294cc3036f1cd89
RomanLitvinchuk <roman67877@gmail.com>
2026-04-05 12:13:55 +0300

    Create class Camera and frustum

--- Lab4/Lab4/camera.h
+++ Lab4/Lab4/camera.h
@@ -1,7 +1,7 @@
 #ifndef CAMERA_H_
 #define CAMERA_H_
 #include <SimpleMath.h>
-
+#include <DirectXCollision.h>
 
 
 struct HullBuffer {
@@ -12,6 +12,20 @@ struct HullBuffer {
 	float gMinDist;
 	float gMaxDist;
 	float padding;
+};
+
+struct Camera {
+	BoundingFrustum frustum_;
+	Matrix projection_;
+	float mCameraYaw;
+	float mCameraPitch;
+	Vector3 mCameraPos = { 15.0f, 20.0f, -90.0f };
+	Vector3 mCameraTarget = { 0.0f, 0.0f, 1.0f };
+	Vector3 mCameraUp = { 0.0f, 1.0f, 0.0f };
+	float mCameraSpeed = 200.0f;
+	float mCameraRotationSpeed = 0.3f;
+	float mRadius_ = 5.0f;
+
 };
 
 #endif //CAMERA_H_
--- Lab4/Lab4/winmain.cpp
+++ Lab4/Lab4/winmain.cpp
@@ -154,7 +154,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	MyFramework.SetViewport();
 	MyFramework.SetScissor();
 	MyFramework.BuildLayout();
-	MyFramework.InitProjectionMatrix();
+	MyFramework.InitProjMAndFrustum();
     MyFramework.Parsing();
     MyFramework.BuildBulbGeometry();
 	MyFramework.CreateVertexBuffer();
